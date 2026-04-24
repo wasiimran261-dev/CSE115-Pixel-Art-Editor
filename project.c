37607a5252a301727a61557ab30908e367f4a80e
wasiimran261-dev <wasi.imran.261@northsouth.edu>
2026-04-24 23:18:29 +0600

    Update project.c

--- project.c
+++ project.c
@@ -84,33 +84,23 @@ int main() {
     InitializeGrid(); //Canvas of the PixelArt editor is ready
     int choice;
     
-    while (choice!=5)
+    while (choice!=5) //loop will continue until user chooses option 5 to exit
     {
-       printf("---Pixel Art Editor---\n1.Display Grid\n2.Draw Pixel\n3.Erase Pixel\n4.Fill Grid\n5.Exit\n\nEnter Choice:");
+       printf("\n---Pixel Art Editor---\n1.Display Grid\n2.Draw Pixel\n3.Erase Pixel\n4.Fill Grid\n5.Exit\n\nEnter Choice:");
        scanf("%d", &choice);
 
        switch (choice){
-        case 1:
-        DisplayGrid();
-        break;
+        case 1: DisplayGrid(); break;
 
-        case 2:
-        DrawPixel();
-        break;
+        case 2: DrawPixel(); break;
 
-        case 3:
-        ErasePixel();
-        break;
+        case 3: ErasePixel(); break;
 
-        case 4:
-        FillGrid();
-        break;
+        case 4: FillGrid(); break;
 
-        case 5:
-        return 0;
+        case 5: return 0;
 
-        default:
-        printf("!!!!Invalid Choice.Try Again!!!!\n");
+        default: printf("!!!!Invalid Choice.Try Again!!!!\n");
        }
        
     }
